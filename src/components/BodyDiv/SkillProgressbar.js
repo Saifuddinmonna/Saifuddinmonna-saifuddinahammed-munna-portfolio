bf41f41bae55710178d1eb9f8961e88c430c1c1e
Saifuddinmonna <Saifuddinmonna@gmail.com>
2023-01-04 15:25:48 +0600

    skil addd actually

--- src/components/BodyDiv/SkillProgressbar.js
+++ src/components/BodyDiv/SkillProgressbar.js
@@ -90,23 +90,65 @@ function StripedExample() {
 						</div>
 					</div>
 					<div className="border transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-200 duration-300 grid  text-justify pt-8 p-2 m-2 rounded-lg">
-						<p className=" p-2 m-2 rounded-lg">
-							{" "}
-							Front end: JavaScript, TypeScript, React, Redux,
-							Redux-Thunk/Saga, Vue, VueX, HTML5, CSS3, Styled
-							components, SCSS, Webpack, Babel.{" "}
-						</p>
-						<p className=" p-2 m-2 rounded-lg">
-							Back end: Node.js, Express.js, MongoDB, Mongoose,
-							Nodemailer, Node-Geocoder, Stripe, Razorpay, Paypal
-							API, Coinbase commerce,{" "}
-						</p>
-						<p className=" p-2 m-2 rounded-lg">
-							DevOps: Git, Github, Heroku, Digital Ocean,
-							Cloudinary, AWS S3, React Dev Tools, Redux Dev
-							tools, SSH, Unit testing, Intergration testing,
-							Continous Integration/Continous Deployment.{" "}
-						</p>
+						<div className="border p-2 m-2 rounded-lg  hover:bg-indigo-400 duration-300 grid">
+							<h1>Professional Skills</h1>
+							<p className=" p-2 m-2 rounded-lg">
+								{" "}
+								<h4 className="text-strong text-bold text-decoration-underline ">
+									{" "}
+									Expertise:
+								</h4>
+								<p>
+									JavaScript, ES6, Rest API, ReactJs, React
+									Router, React Hook, Html5, CSS3, CSS,
+									TailwindCss(DaisyUi and familiar with
+									Headless ) Bootstrap-5, Npm Packages,
+								</p>
+								<h4 className="text-strong text-bold text-decoration-underline ">
+									Comfortable:
+								</h4>
+								<p>
+									Node.js, MongoDb, Firebase, Material UI,
+									ExpressJs, Redux,
+									Next.Js,Typescript,Prticle.js
+								</p>
+								<h4 className="text-strong text-bold text-decoration-underline ">
+									Familiar:
+								</h4>
+								<p>
+									Node Mailer Tools: Github, VS Code ,
+									Brackets, Chrome Dev Tools, Heroku, Netlify,
+									Postman, Photoshop, Figma,Illustrator etc.
+								</p>
+							</p>
+						</div>
+						<div className="border p-2 m-2 rounded-lg  hover:bg-indigo-400 duration-300 grid">
+							<h4 className="text-strong text-bold text-decoration-underline ">
+								Front end:
+							</h4>
+							<p>
+								JavaScript, TypeScript, React, Redux,
+								Redux-Thunk/Saga, HTML5, CSS3, Styled
+								components, SCSS, .
+							</p>
+							<h4 className="text-strong text-bold text-decoration-underline ">
+								Back end:
+							</h4>
+							<p className=" p-2 m-2 rounded-lg">
+								Node.js, Express.js, MongoDB, Mongoose,
+								Nodemailer, Node-Geocoder, Stripe, Paypal API,
+								SSL commerce,{" "}
+							</p>
+							<h4 className="text-strong text-bold text-decoration-underline ">
+								DevOps:
+							</h4>
+							<p className=" p-2 m-2 rounded-lg">
+								Git, Github, React Dev Tools, Redux Dev tools,
+								Tools: Github, VS Code , Brackets, Chrome Dev
+								Tools, Heroku, Netlify, Postman, Photoshop,
+								Figma,Illustrator etc. .{" "}
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
