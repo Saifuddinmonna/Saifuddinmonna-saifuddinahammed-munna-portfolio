7967faf5ce1fed277b488d2e9a4a750b3edb3c9e
Saifuddinmonna <Saifuddinmonna@gmail.com>
2023-01-08 03:45:04 +0600

    search option added

--- src/components/MyPortfolios/MyPortfolioLayout/PortfolioLayout.js
+++ src/components/MyPortfolios/MyPortfolioLayout/PortfolioLayout.js
@@ -5,9 +5,10 @@ import "react-photo-view/dist/react-photo-view.css";
 import NavbarPage2 from "../../NavbarPage/NavbarPage2";
 import MyPortfolios from "../MyPortfolios";
 
-// import portfolios from "../portfolios.json";
+import portfoliosName from "../portfolios.json";
 
 const PortfolioLayout = () => {
+	const [datasName, setDatasName] = useState(portfoliosName);
 	const [datas, setDatas] = useState([]);
 	const [data, setData] = useState();
 	const [imagesState, setImagesState] = useState([]);
@@ -20,6 +21,14 @@ const PortfolioLayout = () => {
 		}
 	};
 
+	const dataFilter = (props) => {
+		// const { name } = name;
+		const newData = datas.filter((data) => data.name == props);
+		 setDatas(newData);
+		console.log(props);
+		console.log(newData);
+	};
+
 	useEffect(() => {
 		Datafuction();
 	}, []);
@@ -29,7 +38,7 @@ const PortfolioLayout = () => {
 			.then((res) => res.json())
 			.then((datas) => setDatas(datas));
 		console.log(datas);
-	}, []);
+	}, [dataFilter,]);
 	console.log(datas);
 	console.log(data);
 	console.log(data?.name);
@@ -38,162 +47,199 @@ const PortfolioLayout = () => {
 		<div>
 			<div className="rounded-2xl border p-2  text-gray-600 body-font">
 				<NavbarPage2></NavbarPage2>
-				{datas &&
-					datas?.map((p) => (
-						<div className="container p-3 m-5 shadow-xl border rounded-xl mx-auto">
-							<section className="text-gray-600 body-font">
-								<div className="container p-3 m-3 mx-auto">
-									<h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
-										{p.category}
-										<br className="hidden sm:block" />
-										{p.name}
-									</h1>
-									<div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-										<div className="p-4 md:w-1/3 flex">
-											<div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"></div>
-											<div className="flex-grow pl-6">
-												<h2 className="text-gray-900 text-left text-2xl text-strong title-font font-medium mb-2">
-													Website Link
-												</h2>
-												<p className="text-left btn btn-warning  btn-sm d-block  text-white text-xl capitalize  ">
-													<a
-														className="text-decoration-none"
-														target="_blank"
-														href={p.liveWebsite}
-														rel="noreferrer">
-														Live Website
-													</a>
-												</p>
-												<p className="text-left btn btn-warning  btn-sm d-block text-white text-xl ">
-													<a
-														className="text-decoration-none"
-														target="_blank"
-														href={p.liveWebsiteRepo}
-														rel="noreferrer">
-														Live Website Repo
-													</a>
-												</p>
-												<p className="text-left btn btn-warning  btn-sm d-block text-white text-xl ">
-													<a
-														className="text-decoration-none"
-														target="_blank"
-														href={p.liveServersite}
-														rel="noreferrer">
-														Live Serversite
-													</a>
-												</p>
-												<p className="text-left btn btn-warning  btn-sm d-block text-white text-xl ">
-													<a
-														className="text-decoration-none"
-														target="_blank"
-														href={
-															p.liveServersiteRepo
-														}
-														rel="noreferrer">
-														LiveServer Site Repo
-													</a>
-												</p>
-											</div>
-										</div>
-										<div className="p-4 md:w-1/3 flex">
-											<div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"></div>
-											<div className="flex-grow pl-6">
-												<h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-													Used Technologies
-												</h2>
-												<p className="leading-relaxed text-base">
-													{p.technology}
-												</p>
-											</div>
-										</div>
-										<div className="p-4 md:w-1/3 flex">
-											<div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
-												<svg
-													fill="none"
-													stroke="currentColor"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													strokeWidth={2}
-													className="w-6 h-6"
-													viewBox="0 0 24 24">
-													<path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
-													<circle
-														cx={12}
-														cy={7}
-														r={4}
-													/>
-												</svg>
-											</div>
-											<div className="flex-grow pl-6">
-												<h2 className="text-gray-900 text-lg title-font font-medium mb-2">
-													Overview
-												</h2>
-												{showMore ? (
-													<>
-														{p.overview
-															.slice(0, 5)
-															.map((over) => (
-																<p className="text-left ">
-																	{over}
-																</p>
-															))}
-													</>
-												) : (
-													<>
-														{p.overview
-															.slice(0, 2)
-															.map((over) => (
-																<p className="text-left ">
-																	{over}
-																</p>
-															))}
-													</>
-												)}
+				<div>
+					<div>
+						{datasName?.map((nameall) => (
+							<div>
+								<button
+									onClick={() => dataFilter(nameall.name)}
+									className="btn btn-warning d-block  w-full m-2 p-2">
+									{nameall.name}
+								</button>
+							</div>
+						))}
+					</div>
+					<div>
+						{datas &&
+							datas?.map((p) => (
+								<div className="container p-3 m-5 shadow-xl border rounded-xl mx-auto">
+									<section className="text-gray-600 body-font">
+										<div className="container p-3 m-3 mx-auto">
+											<h1 className="sm:text-3xl text-2xl font-medium title-font text-center text-gray-900 mb-20">
+												{p.category}
+												<br className="hidden sm:block" />
+												{p.name}
+											</h1>
+											<div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+												<div className="p-4 md:w-1/3 flex">
+													<div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"></div>
+													<div className="flex-grow pl-6">
+														<h2 className="text-gray-900 text-left text-2xl text-strong title-font font-medium mb-2">
+															Website Link
+														</h2>
+														<p className="text-left btn btn-warning  btn-sm d-block  text-white text-xl capitalize  ">
+															<a
+																className="text-decoration-none"
+																target="_blank"
+																href={
+																	p.liveWebsite
+																}
+																rel="noreferrer">
+																Live Website
+															</a>
+														</p>
+														<p className="text-left btn btn-warning  btn-sm d-block text-white text-xl ">
+															<a
+																className="text-decoration-none"
+																target="_blank"
+																href={
+																	p.liveWebsiteRepo
+																}
+																rel="noreferrer">
+																Live Website
+																Repo
+															</a>
+														</p>
+														<p className="text-left btn btn-warning  btn-sm d-block text-white text-xl ">
+															<a
+																className="text-decoration-none"
+																target="_blank"
+																href={
+																	p.liveServersite
+																}
+																rel="noreferrer">
+																Live Serversite
+															</a>
+														</p>
+														<p className="text-left btn btn-warning  btn-sm d-block text-white text-xl ">
+															<a
+																className="text-decoration-none"
+																target="_blank"
+																href={
+																	p.liveServersiteRepo
+																}
+																rel="noreferrer">
+																LiveServer Site
+																Repo
+															</a>
+														</p>
+													</div>
+												</div>
+												<div className="p-4 md:w-1/3 flex">
+													<div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0"></div>
+													<div className="flex-grow pl-6">
+														<h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+															Used Technologies
+														</h2>
+														<p className="leading-relaxed text-base">
+															{p.technology}
+														</p>
+													</div>
+												</div>
+												<div className="p-4 md:w-1/3 flex">
+													<div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4 flex-shrink-0">
+														<svg
+															fill="none"
+															stroke="currentColor"
+															strokeLinecap="round"
+															strokeLinejoin="round"
+															strokeWidth={2}
+															className="w-6 h-6"
+															viewBox="0 0 24 24">
+															<path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+															<circle
+																cx={12}
+																cy={7}
+																r={4}
+															/>
+														</svg>
+													</div>
+													<div className="flex-grow pl-6">
+														<h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+															Overview
+														</h2>
+														{showMore ? (
+															<>
+																{p.overview
+																	.slice(0, 5)
+																	.map(
+																		(
+																			over,
+																		) => (
+																			<p className="text-left ">
+																				{
+																					over
+																				}
+																			</p>
+																		),
+																	)}
+															</>
+														) : (
+															<>
+																{p.overview
+																	.slice(0, 2)
+																	.map(
+																		(
+																			over,
+																		) => (
+																			<p className="text-left ">
+																				{
+																					over
+																				}
+																			</p>
+																		),
+																	)}
+															</>
+														)}
 
-												<button
-													className="btn btn-danger"
-													onClick={() =>
-														setShowMore(!showMore)
-													}>
-													{showMore
-														? "Show less"
-														: "Show more"}
-													<svg
-														fill="none"
-														stroke="currentColor"
-														strokeLinecap="round"
-														strokeLinejoin="round"
-														strokeWidth={2}
-														className="w-4 h-4 ml-2"
-														viewBox="0 0 24 24">
-														<path d="M5 12h14M12 5l7 7-7 7" />
-													</svg>
-												</button>
+														<button
+															className="btn btn-danger"
+															onClick={() =>
+																setShowMore(
+																	!showMore,
+																)
+															}>
+															{showMore
+																? "Show less"
+																: "Show more"}
+															<svg
+																fill="none"
+																stroke="currentColor"
+																strokeLinecap="round"
+																strokeLinejoin="round"
+																strokeWidth={2}
+																className="w-4 h-4 ml-2"
+																viewBox="0 0 24 24">
+																<path d="M5 12h14M12 5l7 7-7 7" />
+															</svg>
+														</button>
+													</div>
+												</div>
 											</div>
 										</div>
+									</section>
+
+									<div className="grid grid-cols-3">
+										{p?.image?.map((imgs, ind) => (
+											<div key={ind} className="">
+												<div className=" border m-2 p-2 rounded transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-150 hover:bg-indigo-500 duration-300">
+													<PhotoProvider>
+														<PhotoView
+															src={`images/${imgs}`}>
+															<img
+																src={`images/${imgs}`}
+																alt="Pic of portfolios"
+															/>
+														</PhotoView>
+													</PhotoProvider>
+												</div>
+											</div>
+										))}
 									</div>
 								</div>
-							</section>
-
-							<div className="grid grid-cols-3">
-								{p?.image?.map((imgs, ind) => (
-									<div key={ind} className="">
-										<div className=" border m-2 p-2 rounded transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-150 hover:bg-indigo-500 duration-300">
-											<PhotoProvider>
-												<PhotoView
-													src={`images/${imgs}`}>
-													<img
-														src={`images/${imgs}`}
-														alt="Pic of portfolios"
-													/>
-												</PhotoView>
-											</PhotoProvider>
-										</div>
-									</div>
-								))}
-							</div>
-						</div>
-					))}
+							))}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
